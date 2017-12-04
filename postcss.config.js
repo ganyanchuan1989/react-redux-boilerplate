3f80f942a602a375eb2e3be1ba18d19811126139
ganxunzou <ganxunzou@163.com>
2017-12-04 23:04:25 +0800

    add

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,10 @@
 module.exports = {
-  // Note that PostCSS plugin ordering is important.
-  plugins: [
-    require('postcss-nested')(),
-    require('postcss-import')({
-      path: [
-        './src'
-      ],
-    }),
-    require('postcss-custom-properties')(),
-    require('postcss-color-function')(),
-    require('autoprefixer')({ browsers: ['last 2 versions'] }),
-  ]
+  parser: 'postcss-less',
+  plugins: {
+		'postcss-nested':{},
+		'postcss-import': {path: ['./src']},
+    'postcss-cssnext': {},
+		'cssnano': {},
+		'autoprefixer':{},
+  }
 }
